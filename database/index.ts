24d6d87022d66866705b4acae20268545e298d48
esvobodova <eliska.svobodova.pdy@seznam.cz>
2026-05-16 10:43:36 +0200

    App running in the browser as well on mobile

--- database/index.ts
+++ database/index.ts
@@ -1,2 +1,2 @@
-export { db } from './client';
+export { getDatabase, initDatabase, type AppDatabase } from './client';
 export * from './schema';
--- metro.config.js
+++ metro.config.js
@@ -3,5 +3,15 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 config.resolver.sourceExts.push('sql');
+config.resolver.assetExts.push('wasm');
+
+// Required for expo-sqlite on web (SharedArrayBuffer + wa-sqlite.wasm)
+config.server.enhanceMiddleware = (middleware) => {
+  return (req, res, next) => {
+    res.setHeader('Cross-Origin-Embedder-Policy', 'credentialless');
+    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+    return middleware(req, res, next);
+  };
+};
 
 module.exports = withNativeWind(config, { input: './global.css' });
